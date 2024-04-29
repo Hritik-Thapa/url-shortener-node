200ebef8a660aaaa73d661993e0d028db2939aee
Hritik-Thapa <hritikthapa331@gmail.com>
2024-04-29 23:09:01 +0545

    added ip to analytics

--- controllers/url.js
+++ controllers/url.js
@@ -2,23 +2,23 @@ const url = require("../models/url");
 const shortid = require("shortid");
 
 async function handleGenerateShortId(req, res) {
+    console.log('Inside Generate Short Id');
   const body = req.body.url;
   if (!body) return res.status(400).json({ error: "Missing data" });
   const short_id = shortid(8);
-  // console.log(shortId);
   await url.create({
     shortId: short_id,
     redirectUrl: body,
     visitHistory: [],
   });
-
   return res.json({ id: short_id });
 }
 
 async function handleRedirectUrl(req, res) {
+    console.log(req.ip)
     const shortId= req.params.shortId;
   const entry = await url.findOneAndUpdate({shortId}, {
-    $push: { visitHistory: {timestamp:Date.now()} },
+    $push: { visitHistory: {timestamp:Date.now(),ipAddress:req.ip} },
   });
   console.log(entry)
   res.redirect(entry.redirectUrl);
